81f0ccf920e9d125c65c1f05057df166ab3b74f0
olegov-lab <olego8760@gmail.com>
2021-01-31 18:14:18 +0300

    fix: logic

--- BinarySearch.js
+++ BinarySearch.js
@@ -1,10 +1,25 @@
-function searchedBinary(array) {
-let low = 0;
-let high = array.length - 1;
-let current = 0;
+//The binary search in a sorting list
+function searchedBinary(list, item) {
+       let low = 0;
+       let high = list.length - 1;
+       let guess = 0;
 
-while (low <= high) {
-mid = (low + high) / 2;
-current = array[mid];
-}
-}
+       while (low <= high) {
+       let mid = Math.floor((low + high) / 2);
+       guess = list[mid];
+
+       if (guess == item) {
+           return guess;
+       } else if (guess > item) {
+              high = mid - 1;
+       } else if (guess < item) {
+              low = mid +1;
+       } else {
+         return null;
+         }
+       }
+       }
+
+
+       let list = [1,2,3,4];
+       console.log(searchedBinary(list, 3));
